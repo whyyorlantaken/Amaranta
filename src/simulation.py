6bd4f5a7ba6314419fc77d5843b0c42270df853b
Mariannly <mariannly.merquez@gmail.com>
2026-03-10 18:06:07 -0500

    spatial gif test

--- src/simulation.py
+++ src/simulation.py
@@ -1,16 +1,3 @@
-# initialize
-    # read array with initial conditions from data or user imput
-    # read parameters (read config)
-    #initialize somthing to save states population (total de zeros, unos y dos) and images (in array) (for visualization)
-
-#LOOP
-    # use getMooreCounts
-    # update epsilon and probs (?)
-    # update states (time evolution) (applying rules: deterministic and then stochastic or the other way around?)
-    # save states population and images (arrays) (for visualization)
-
-# TODO: Create a folder per run: ChaosMode-Value_dist-DistributionType (images and evolution)
-
 # config = {
 
 #     'simulation': {
